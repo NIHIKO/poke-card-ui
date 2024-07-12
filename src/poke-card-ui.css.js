c39e8a1ac795c9af523a9475a0fa534f64766051
Diego Andres Mateus Triana <diego.mateus@novatec.com.co>
2024-07-12 14:15:36 -0500

    Update Styles

--- src/poke-card-ui.css.js
+++ src/poke-card-ui.css.js
@@ -2,78 +2,70 @@ import { css, unsafeCSS } from "lit-element";
 import * as foundations from "@bbva-web-components/bbva-foundations-styles";
 
 export default css`
-  :host {
-    display: block;
-    box-sizing: border-box;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  }
+:host {
+  display: flex;
+  box-sizing: border-box;
+  justify-content: center;
+  width: 100%;
+  max-width: 1024px;
+  margin: 0 auto;
+}
 
-  :host([hidden]),
-  [hidden] {
-    display: none !important;
-  }
+:host([hidden]), [hidden] {
+  display: none !important;
+}
 
-  *,
-  *::before,
-  *::after {
-    box-sizing: inherit;
-  }
+*, *::before, *::after {
+  box-sizing: inherit;
+}
 
-  .card {
-    width: 300px;
-    padding: 20px;
-    border-radius: 10px;
-    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
-    transition: 0.3s;
-    background-color: #fff;
-  }
+.img-pokemon {
+  width: 15rem;
+}
 
-  .card:hover {
-    box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2);
-  }
+.card {
+  width: 300px;
+  padding: 20px;
+  margin: 10px;
+  border-radius: 10px;
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+  transition: 0.3s;
+}
 
-  .img-pokemon {
-    width: 100%;
-    height: auto;
-    border-radius: 5px 5px 0 0;
-  }
+.card:hover {
+  box-shadow: 0 30px 16px 0 rgba(0, 0, 0, 0.2);
+}
 
-  .container {
-    padding: 2px 16px;
-    text-align: center;
-  }
+.container {
+  padding: 2px 16px;
+  text-align: center;
+}
 
-  h2 {
-    font-size: 1.5em;
-    margin: 10px 0;
-  }
+h2 {
+  font-size: 1.5em;
+  margin: 10px 0;
+  text-align: center;
+}
 
-  .stats {
-    display: flex;
-    justify-content: space-between;
-    flex-wrap: wrap;
-    margin-top: 10px;
-  }
+.stats {
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: wrap;
+  margin-top: 10px;
+}
 
-  .stat {
-    text-align: center;
-    width: 45%;
-    margin-bottom: 10px;
-  }
+.stat {
+  text-align: center;
+  width: 45%;
+  margin-bottom: 10px;
+}
 
-  .stat span {
-    display: block;
-  }
+.stat span {
+  display: block;
+}
 
-  .stat .label {
-    color: gray;
-    font-size: 0.8em;
-  }
-
-  .stat .value {
-    font-size: 1.2em;
-    font-weight: bold;
-  }
+.stat .label {
+  color: gray;
+  font-size: 0.8em;
+}
 `;
